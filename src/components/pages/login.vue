7deeb2d4541c104fe1d0f8bde09955c3f6519dc3
cmtj199337 <cmtj199337@163.com>
2017-07-05 17:56:33 +0800

    init

---
+++ src/components/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="login">
+		<form action="">
+			<input type="text" name="">
+			<input type="password" name="">
+			<input type="submit" value="登录">
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+	  	name: 'login',
+	 	 data () {
+		    return {
+		      
+		    }
+	  	}
+	}
+</script>
