e947224fc1d604ec8acd700b4ac57ba4aad5493e
cmtj199337 <cmtj199337@163.com>
2017-07-06 18:43:14 +0800

    $

--- src/router/index.js
+++ src/router/index.js
@@ -7,30 +7,48 @@ Vue.use(Router)
 //创建路由实例并配置路由映射
 //path：'*'，redirect:'/home'    重定向到path是/home的映射
 const router = new Router({
-	routes:[{
-		path: '*',redirect:'/login'
-	},{
-		path: '/movie',component:require('../components/movie.vue')
-	},{
-		path: '/login',component:require('../components/login/login.vue')
-	},{
-		path: '/hello',component:require('../components/Hello.vue')
-	},{
-		path: '/forgetpass',component:require('../components/login/forgetpass.vue')
-	},{
-		path: '/main',
+	routes:[
+		{
+			path: '*',
+			redirect:'/login'
+		},
+		{
+			path: '/movie',
+			component:require('../components/movie.vue')
+		},
+		{
+			path: '/login',
+			component:require('../components/login/login.vue')
+		},
+		{
+			path: '/hello',
+			component:require('../components/Hello.vue')
+		},
+		{
+			path: '/forgetpass',
+			component:require('../components/login/forgetpass.vue')
+		},
+		{
+			path: '/main',
 			meta:{
 				//添加该字段，表示进入这个路由是需要登录的
 				requireAuth: true,
 			},
-		component:require('../components/main.vue')
-	},{
-		path: '/register',component:require('../components/register/register.vue')
-	},{
-		path: '/iregister',component:require('../components/register/iRegister.vue')
-	},{
-		path: '/tregister',component:require('../components/register/tRegister.vue')
-	}]
+			component:require('../components/main.vue')
+		},
+		{
+			path: '/register',
+			component:require('../components/register/register.vue')
+		},
+		{
+			path: '/iregister',
+			component:require('../components/register/iRegister.vue')
+		},
+		{
+			path: '/tregister',
+			component:require('../components/register/tRegister.vue')
+		}
+	]
 })
 
 
